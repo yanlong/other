528e1a953f0e02f7f2ec4d1c58c032c97a04e98f
yanlong <scicodist@gmail.com>
2015-02-13 00:31:36 +0800

    Add git pretty log alias

--- git/config.sh
+++ git/config.sh
@@ -5,3 +5,8 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.br branch
 git config --global alias.di diff
+git config --global alias.log1 'log --graph --abbrev-commit --decorate --date=relative --all' --replace-all
+git config --global alias.log2 'log --graph --oneline --decorate --date=relative --all' --replace-all
+
+
+
