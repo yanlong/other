8215625a5b5dbf2cbdd96cd4a83366d863406e70
yanlong <scicodist@gmail.com>
2015-06-10 20:06:17 +0800

    Up

--- mongodb/crontab.sh
+++ mongodb/crontab.sh
@@ -1,3 +1,2 @@
-
-10 04 * * * /shell/mongobak 1>/log/crontab_mongo_back.file &
-10 02 * * * /shell/mongobakdelete 1>/log/crontab_mongo_delete.file &
+40 19 * * * /root/other/mongodb/backup.sh 1>/var/log/crontab_mongo_back.file &
+43 19 * * * /root/other/mongodb/restore.sh 1>/var/log/crontab_mongo_delete.file &
