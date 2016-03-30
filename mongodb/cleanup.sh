8c3fff69b7553b45bcab987fa58bb2b884a279d8
yanlong <yanlong@baidu.com>
2016-03-30 21:06:53 +0800

    up

--- mongodb/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-targetpath='/var/backup/mongobak'
-nowtime=$(date -d '-7 days' "+%Y%m%d")
-if [ -d "${targetpath}/${nowtime}/" ]
-then
-  rm -rf "${targetpath}/${nowtime}/"
-  echo "=======${targetpath}/${nowtime}/===删除完毕=="
-fi
-echo "===$nowtime ==="
--- mongodb/crontab.sh
+++
@@ -1,2 +0,0 @@
-40 19 * * * /root/other/mongodb/backup.sh 1>/var/log/crontab_mongo_back.file &
-43 19 * * * /root/other/mongodb/restore.sh 1>/var/log/crontab_mongo_delete.file &
